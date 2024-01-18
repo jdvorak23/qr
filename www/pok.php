21d5997194c8f2986d61c500a7fcb14a808906e0
jdvorak23 <dvorakj3@post.cz>
2024-01-18 14:20:14 +0100

    cleaning

--- www/pok.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-ini_set('display_startup_errors', 1);
-ini_set('display_errors', 1);
-error_reporting(-1);
-require_once '../vendor/autoload.php';
-
-echo urlencode('SPD*1.0*ACC:CZ5120100000002600983361*AM:31138*CC:CZK*DT:20240117*MSG:Jana Valchová*X-VS:424060096');
