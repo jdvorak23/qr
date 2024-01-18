30a4270a98b8c270200dc66de7d6a36d83fe10ba
jdvorak23 <dvorakj3@post.cz>
2024-01-18 14:19:18 +0100

    funkcni

--- www/index.php
+++ www/index.php
@@ -1,35 +1,144 @@
 <?php
 
-use chillerlan\QRCode\Output\QROutputInterface;
-use chillerlan\QRCode\QRCode;
-use chillerlan\QRCode\QROptions;
+use Endroid\QrCode\Builder\Builder;
+use Endroid\QrCode\Encoding\Encoding;
+use Endroid\QrCode\ErrorCorrectionLevel;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
 
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
-require_once '../vendor/autoload.php';
 
-function debug(mixed $value, $dump = false)
+require_once '../vendor/autoload.php';
+const defaultWidth = 150;
+const defaultHeight = 150;
+const defaultMargin = 4;
+
+const defaultCorrectionLevel = ErrorCorrectionLevel::Low;
+
+const defaultCoding = 'UTF-8';
+
+/**
+ * Při jakékoli chybě zobrazí prázdný obrázek, což je aktuálně bílá plocha
+ * width a height jsou z parametrů, pokud chybí v parametru bere se defaultní
+ * @param int $width
+ * @param int $height
+ * @return void
+ */
+function showErrorImage(int $width, int $height)
 {
-	echo '<pre>';
-	if ($dump)
-		var_dump($value);
-	else
-		print_r($value);
-	echo '</pre>';
+	$whiteImage = imagecreatetruecolor($width, $height);
+	$white = imagecolorallocate($whiteImage, 255, 255, 255);
+	imagefill($whiteImage,0,0, $white);
+	header('Content-Type: image/png');
+	imagepng($whiteImage);
+	exit;
 }
 
-$data = 'otpauth://totp/test?secret=B3JX4VCVJDVNXNZ5&issuer=chillerlan.net';
+/**
+ * Vytvoří a zobrazí qr kód
+ * @param string $data Co má výt zakódováno v QR kódu
+ * @param int $width
+ * @param int $height
+ * @param ErrorCorrectionLevel $correctionLevel Zkusit s "low" když jsou $data příliš velká pro vyšší korekci
+ * @param int $margin Margin okolo obrázku, snížit na 0 pokud jsou moc velká $data
+ * @return void
+ */
+function showQrImage(string $data, int $width, int $height, ErrorCorrectionLevel $correctionLevel, int $margin, string $coding)
+{
+	// Knihovna kreslí pouze čtvercové qr kódy, vezmeme menší z width a height a odečteme margin
+	$size = min($width, $height) - 2 * $margin;
+	// Vytvoření qr kódu
+	$qr = Builder::create()
+		->writer(new PngWriter())
+		->writerOptions([])
+		->data($data)
+		->encoding(new Encoding($coding)) //'ISO-8859-1' - lepší kompatibilita ale nezvládá české znaky apod.
+		->errorCorrectionLevel($correctionLevel) // Metodou pokus / omyl aby byly co nejpodobnější googlu
+		->size($size)
+		->margin($margin)
+		->roundBlockSizeMode(RoundBlockSizeMode::Margin)
+		->validateResult(false)
+		->build();
+	// Pokud je width === height, máme hotovo jen zobrazíme
+	if ($width === $height) {
+		header('Content-Type: image/png');
+		echo $qr->getString();
+	} else { // Pokud width !== height, přidáme bílé místo kam je potřeba
+		$image = imagecreatefromstring($qr->getString());
+		$sizedImage = imagecreatetruecolor($width, $height);
+		$white = imagecolorallocate($sizedImage, 255, 255, 255);
+		imagefill($sizedImage,0,0, $white);
+		imagecopy($sizedImage, $image, round(($width - imagesx($image)) / 2), round(($height - imagesy($image)) / 2) , 0, 0, imagesx($image), imagesy($image));
+		header('Content-Type: image/png');
+		imagepng($sizedImage);
+	}
+}
 
-$options = new QROptions([
-	'returnResource' => true,
-	'outputType' => QROutputInterface::GDIMAGE_PNG//QROutputInterface::GDIMAGE_JPG
-]);
+// Získání parametrů
+$parts = parse_url($_SERVER['REQUEST_URI']);
+parse_str($parts['query'] ?? '', $params);
 
+// Parametr 'chs' který by měl být např. 100x100 - velikost obrázku
+// Zkusíme získat, pokud není parametr, nebo jsou v něm nesmysly (není číslo, není 'x'), bere defaultní
+// Pokud není část 'x100', ale jen např chs=100, height veme stejné jako width
+if(isset($params['chs'])) {
+	$dims = explode('x', trim($params['chs']));
+	if ($dims[0] === (string)(int) $dims[0]) {
+		$width = (int) $dims[0];
+	} else {
+		$width = defaultWidth;
+	}
+	if (isset($dims[1]) && $dims[1] === (string)(int) $dims[1]) {
+		$height = (int) $dims[1];
+	} else {
+		$height = $width;
+	}
+} else {
+	$width = defaultWidth;
+	$height = defaultHeight;
+}
 
+// Parametr 'chl' jsou data, která mají být v obrázku, urlencoded
+// Pokud parametr chybí, zobrazíme defaultní error image
+if( ! isset($params['chl'])) {
+	showErrorImage($width, $height);
+}
+$data = urldecode($params['chl']);
 
-//
+// Parametr 'chld' je error correction level a margin ve formátu 'level|margin'
+// level může být 'L' (default) - low, 'M' - medium, 'Q' - quartile, 'H' - high
+$correctionLevel = defaultCorrectionLevel;
+$margin = defaultMargin;
+if(isset($params['chld'])) {
+	[$level, $insertedMargin] = explode('|', $params['chld']);
+	$correctionLevel = match ($level) {
+		'M' => ErrorCorrectionLevel::Medium,
+		'Q' => ErrorCorrectionLevel::Quartile,
+		'H' => ErrorCorrectionLevel::High,
+		default => ErrorCorrectionLevel::Low
+	};
+	if (isset($insertedMargin) && $insertedMargin === (string)(int) $insertedMargin) {
+		$margin = (int) $insertedMargin;
+	}
+}
+// Kódování defaultně 'UTF-8', lze vybrat 'ISO-8859-1' parametrem 'choe'
+$coding = match ($params['choe'] ?? '') {
+	'ISO-8859-1' => 'ISO-8859-1',
+	default => defaultCoding
+};
 
-$qrCode = new QRCode($options);
-header('Content-Type: image/png');
-imagegd($qrCode->render($data));
+try {
+	// Pokusíme se vytvořit a zobrazit QR k s danými daty
+	showQrImage($data, $width, $height, $correctionLevel, $margin, $coding);
+} catch (Throwable $e) {
+	// Pokud se nepovedlo, nejčastěji kvůli moc dlouhému $data. Snížením $correctionLevel a $margin tam můžeme nacpat víc dat
+	// Nebo coding, někdo zadá ISO a dá tam nepovolené znaky
+	try {
+		showQrImage($data, $width, $height, ErrorCorrectionLevel::Low, 0,  'UTF-8');
+	} catch (Throwable $e) {
+		// Pokud ani to se nepovedlo, error
+		showErrorImage($width, $height);
+	}
+}
--- www/pok.php
+++ www/pok.php
@@ -1,39 +1,8 @@
 <?php
 
-use App\SVGConvert;
-use chillerlan\QRCode\Data\QRMatrix;
-use chillerlan\QRCode\Output\QROutputInterface;
-use chillerlan\QRCode\QRCode;
-use chillerlan\QRCode\QROptions;
-
 ini_set('display_startup_errors', 1);
 ini_set('display_errors', 1);
 error_reporting(-1);
 require_once '../vendor/autoload.php';
 
-// SVG from the basic example
-$options = new QROptions([
-	'version' => 7,
-	'outputInterface' => SVGConvert::class,
-	'imagickFormat' => 'png32',
-	'scale' => 20,
-	'outputBase64' => false,
-	'drawLightModules' => true,
-	'svgUseFillAttributes' => false,
-	'connectPaths' => true,
-	'keepAsSquare' => [
-		QRMatrix::M_FINDER_DARK,
-		QRMatrix::M_FINDER_DOT,
-		QRMatrix::M_ALIGNMENT_DARK,
-	],
-]);
-
-
-
-
-// render the SVG and convert to the desired ImageMagick format
-$image = (new QRCode($options))->render('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-
-//header('Content-type: image/png');
-
-echo $image;
+echo urlencode('SPD*1.0*ACC:CZ5120100000002600983361*AM:31138*CC:CZK*DT:20240117*MSG:Jana Valchová*X-VS:424060096');
